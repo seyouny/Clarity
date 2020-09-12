a25c23eedf9f422c91615cb82c6c872ae131afd5
Seyoung Yoon <stacey.exp@gmail.com>
2020-09-11 21:10:18 -0700

    box wrappers

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import NavBar from "./components/Navbar/Navbar"
+import SimplePaper from "./components/Section"
+import "./style.css" 
 function App() {
   return (
     <div className="App">
+      <div className="navbar">
       <NavBar/>
+      </div>
+      <hr></hr>
+      <div className="paper">
+      <SimplePaper />
+      </div>
     </div>
   );
 }
