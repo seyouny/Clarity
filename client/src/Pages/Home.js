fd7ef8fc7d7acadcbb3b223f000edd92970d9c2c
eddyangang <eddyyang@berkeley.edu>
2020-09-12 19:17:33 -0700

    moved files

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Tabs from "../components/Tabs/Tabs"
+import Speech from "../components/Speech/Speech"
+export default function Home() {
+    return (
+        <div>
+            <Speech/>
+            <Tabs/>
+        </div>
+    )
+}
