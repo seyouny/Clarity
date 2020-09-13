4f1babb2b0e3071240363271ae2270dd57a13403
Seyoung Yoon <stacey.exp@gmail.com>
2020-09-12 21:03:16 -0700

    server.js aagain

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-var express = require('express');
+const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.listen(PORT, function() {
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+// Define API routes here
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+  app.listen(PORT, function() {
     console.log('App listening on PORT ' + 'http://localhost:' + PORT);
   });
